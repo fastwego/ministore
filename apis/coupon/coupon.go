01d86c4de643b910e50df37c0293b4c296228335
citizen233 <citizen233@fastwego.dev>
2020-08-24 00:57:18 +0800

    feat: init

--- apis/coupon/coupon.go
+++ apis/coupon/coupon.go
@@ -1,9 +1,28 @@
+// Copyright 2020 FastWeGo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package coupon 优惠券接口
 package coupon
 
+import (
+	"bytes"
+	"github.com/fastwego/ministore"
+)
+
 const (
-	apiCouponGetCoupon  = "/product/coupon/get_list"
-	apiCouponPushCoupon = "/product/coupon/push"
+	apiGetCoupon  = "/product/coupon/get_list"
+	apiPushCoupon = "/product/coupon/push"
 )
 
 /*
@@ -12,8 +31,8 @@ const (
 See: https://developers.weixin.qq.com/doc/ministore/minishopopencomponent/API/coupon/get_coupon.html
 POST https://api.weixin.qq.com/product/coupon/get_list?access_token=xxxxxxxxx
 */
-func CouponGetCoupon(ctx *ministore.Ministore, payload []byte) (resp []byte, err error) {
-	return ctx.Client.HTTPPost(apiCouponGetCoupon, bytes.NewReader(payload), "application/json;charset=utf-8")
+func GetCoupon(ctx *ministore.MiniStore, payload []byte) (resp []byte, err error) {
+	return ctx.Client.HTTPPost(apiGetCoupon, bytes.NewReader(payload), "application/json;charset=utf-8")
 }
 
 /*
@@ -22,6 +41,6 @@ func CouponGetCoupon(ctx *ministore.Ministore, payload []byte) (resp []byte, err
 See: https://developers.weixin.qq.com/doc/ministore/minishopopencomponent/API/coupon/push_coupon.html
 POST https://api.weixin.qq.com/product/coupon/push?access_token=xxxxxxxxx
 */
-func CouponPushCoupon(ctx *ministore.Ministore, payload []byte) (resp []byte, err error) {
-	return ctx.Client.HTTPPost(apiCouponPushCoupon, bytes.NewReader(payload), "application/json;charset=utf-8")
+func PushCoupon(ctx *ministore.MiniStore, payload []byte) (resp []byte, err error) {
+	return ctx.Client.HTTPPost(apiPushCoupon, bytes.NewReader(payload), "application/json;charset=utf-8")
 }
--- apis/coupon/coupon_test.go
+++ apis/coupon/coupon_test.go
@@ -1,4 +1,27 @@
+// Copyright 2020 FastWeGo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package coupon
+
+import (
+	"github.com/fastwego/ministore"
+	"github.com/fastwego/ministore/test"
+	"net/http"
+	"os"
+	"reflect"
+	"testing"
+)
 
 func TestMain(m *testing.M) {
 	test.Setup()
@@ -10,11 +33,11 @@ func TestCouponGetCoupon(t *testing.T) {
 		"case1": []byte("{\"errcode\":0,\"errmsg\":\"ok\"}"),
 	}
 	var resp []byte
-	test.MockSvrHandler.HandleFunc(apiCouponGetCoupon, func(w http.ResponseWriter, r *http.Request) {
+	test.MockSvrHandler.HandleFunc(apiGetCoupon, func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(resp))
 	})
 	type args struct {
-		ctx     *ministore.Ministore
+		ctx     *ministore.MiniStore
 		payload []byte
 	}
 	tests := []struct {
@@ -23,12 +46,12 @@ func TestCouponGetCoupon(t *testing.T) {
 		wantResp []byte
 		wantErr  bool
 	}{
-		{name: "case1", args: args{ctx: test.MockMinistore}, wantResp: mockResp["case1"], wantErr: false},
+		{name: "case1", args: args{ctx: test.MockMiniStore}, wantResp: mockResp["case1"], wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			resp = mockResp[tt.name]
-			gotResp, err := CouponGetCoupon(tt.args.ctx, tt.args.payload)
+			gotResp, err := GetCoupon(tt.args.ctx, tt.args.payload)
 			//fmt.Println(string(gotResp), err)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CouponGetCoupon() error = %v, wantErr %v", err, tt.wantErr)
@@ -45,11 +68,11 @@ func TestCouponPushCoupon(t *testing.T) {
 		"case1": []byte("{\"errcode\":0,\"errmsg\":\"ok\"}"),
 	}
 	var resp []byte
-	test.MockSvrHandler.HandleFunc(apiCouponPushCoupon, func(w http.ResponseWriter, r *http.Request) {
+	test.MockSvrHandler.HandleFunc(apiPushCoupon, func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(resp))
 	})
 	type args struct {
-		ctx     *ministore.Ministore
+		ctx     *ministore.MiniStore
 		payload []byte
 	}
 	tests := []struct {
@@ -58,12 +81,12 @@ func TestCouponPushCoupon(t *testing.T) {
 		wantResp []byte
 		wantErr  bool
 	}{
-		{name: "case1", args: args{ctx: test.MockMinistore}, wantResp: mockResp["case1"], wantErr: false},
+		{name: "case1", args: args{ctx: test.MockMiniStore}, wantResp: mockResp["case1"], wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			resp = mockResp[tt.name]
-			gotResp, err := CouponPushCoupon(tt.args.ctx, tt.args.payload)
+			gotResp, err := PushCoupon(tt.args.ctx, tt.args.payload)
 			//fmt.Println(string(gotResp), err)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CouponPushCoupon() error = %v, wantErr %v", err, tt.wantErr)
