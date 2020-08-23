b6fa00f66436f4ef440d0c47835078e574d4a202
citizen233 <citizen233@fastwego.dev>
2020-08-24 01:06:02 +0800

    feat: init

--- apis/order/order.go
+++ apis/order/order.go
@@ -1,9 +1,28 @@
+// Copyright 2020 FastWeGo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package order 订单接口
 package order
 
+import (
+	"bytes"
+	"github.com/fastwego/ministore"
+)
+
 const (
-	apiOrderGetOrderList   = "/product/order/get_list"
-	apiOrderGetOrderDetail = "/product/order/get"
+	apiGetOrderList   = "/product/order/get_list"
+	apiGetOrderDetail = "/product/order/get"
 )
 
 /*
@@ -12,8 +31,8 @@ const (
 See: https://developers.weixin.qq.com/doc/ministore/minishopopencomponent/API/order/get_order_list.html
 POST https://api.weixin.qq.com/product/order/get_list?access_token=xxxxxxxxx
 */
-func OrderGetOrderList(ctx *ministore.Ministore, payload []byte) (resp []byte, err error) {
-	return ctx.Client.HTTPPost(apiOrderGetOrderList, bytes.NewReader(payload), "application/json;charset=utf-8")
+func GetOrderList(ctx *ministore.MiniStore, payload []byte) (resp []byte, err error) {
+	return ctx.Client.HTTPPost(apiGetOrderList, bytes.NewReader(payload), "application/json;charset=utf-8")
 }
 
 /*
@@ -22,6 +41,6 @@ func OrderGetOrderList(ctx *ministore.Ministore, payload []byte) (resp []byte, e
 See: https://developers.weixin.qq.com/doc/ministore/minishopopencomponent/API/order/get_order_detail.html
 POST https://api.weixin.qq.com/product/order/get?access_token=xxxxxxxxx
 */
-func OrderGetOrderDetail(ctx *ministore.Ministore, payload []byte) (resp []byte, err error) {
-	return ctx.Client.HTTPPost(apiOrderGetOrderDetail, bytes.NewReader(payload), "application/json;charset=utf-8")
+func GetOrderDetail(ctx *ministore.MiniStore, payload []byte) (resp []byte, err error) {
+	return ctx.Client.HTTPPost(apiGetOrderDetail, bytes.NewReader(payload), "application/json;charset=utf-8")
 }
--- apis/order/order_test.go
+++ apis/order/order_test.go
@@ -1,4 +1,27 @@
+// Copyright 2020 FastWeGo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package order
+
+import (
+	"github.com/fastwego/ministore"
+	"github.com/fastwego/ministore/test"
+	"net/http"
+	"os"
+	"reflect"
+	"testing"
+)
 
 func TestMain(m *testing.M) {
 	test.Setup()
@@ -10,11 +33,11 @@ func TestOrderGetOrderList(t *testing.T) {
 		"case1": []byte("{\"errcode\":0,\"errmsg\":\"ok\"}"),
 	}
 	var resp []byte
-	test.MockSvrHandler.HandleFunc(apiOrderGetOrderList, func(w http.ResponseWriter, r *http.Request) {
+	test.MockSvrHandler.HandleFunc(apiGetOrderList, func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(resp))
 	})
 	type args struct {
-		ctx     *ministore.Ministore
+		ctx     *ministore.MiniStore
 		payload []byte
 	}
 	tests := []struct {
@@ -23,19 +46,19 @@ func TestOrderGetOrderList(t *testing.T) {
 		wantResp []byte
 		wantErr  bool
 	}{
-		{name: "case1", args: args{ctx: test.MockMinistore}, wantResp: mockResp["case1"], wantErr: false},
+		{name: "case1", args: args{ctx: test.MockMiniStore}, wantResp: mockResp["case1"], wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			resp = mockResp[tt.name]
-			gotResp, err := OrderGetOrderList(tt.args.ctx, tt.args.payload)
+			gotResp, err := GetOrderList(tt.args.ctx, tt.args.payload)
 			//fmt.Println(string(gotResp), err)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("OrderGetOrderList() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GetOrderList() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(gotResp, tt.wantResp) {
-				t.Errorf("OrderGetOrderList() gotResp = %v, want %v", gotResp, tt.wantResp)
+				t.Errorf("GetOrderList() gotResp = %v, want %v", gotResp, tt.wantResp)
 			}
 		})
 	}
@@ -45,11 +68,11 @@ func TestOrderGetOrderDetail(t *testing.T) {
 		"case1": []byte("{\"errcode\":0,\"errmsg\":\"ok\"}"),
 	}
 	var resp []byte
-	test.MockSvrHandler.HandleFunc(apiOrderGetOrderDetail, func(w http.ResponseWriter, r *http.Request) {
+	test.MockSvrHandler.HandleFunc(apiGetOrderDetail, func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(resp))
 	})
 	type args struct {
-		ctx     *ministore.Ministore
+		ctx     *ministore.MiniStore
 		payload []byte
 	}
 	tests := []struct {
@@ -58,19 +81,19 @@ func TestOrderGetOrderDetail(t *testing.T) {
 		wantResp []byte
 		wantErr  bool
 	}{
-		{name: "case1", args: args{ctx: test.MockMinistore}, wantResp: mockResp["case1"], wantErr: false},
+		{name: "case1", args: args{ctx: test.MockMiniStore}, wantResp: mockResp["case1"], wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			resp = mockResp[tt.name]
-			gotResp, err := OrderGetOrderDetail(tt.args.ctx, tt.args.payload)
+			gotResp, err := GetOrderDetail(tt.args.ctx, tt.args.payload)
 			//fmt.Println(string(gotResp), err)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("OrderGetOrderDetail() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GetOrderDetail() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(gotResp, tt.wantResp) {
-				t.Errorf("OrderGetOrderDetail() gotResp = %v, want %v", gotResp, tt.wantResp)
+				t.Errorf("GetOrderDetail() gotResp = %v, want %v", gotResp, tt.wantResp)
 			}
 		})
 	}
