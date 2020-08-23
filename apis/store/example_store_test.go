2a6c3784a81850443585d2d253416f72d84f0d0d
citizen233 <citizen233@fastwego.dev>
2020-08-24 01:15:21 +0800

    feat: init

--- apis/store/example_store_test.go
+++ apis/store/example_store_test.go
@@ -1,8 +1,28 @@
+// Copyright 2020 FastWeGo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package store_test
 
+import (
+	"fmt"
+	"github.com/fastwego/ministore"
+	"github.com/fastwego/ministore/apis/store"
+)
+
 func ExampleStoreGetStoreInfo() {
-	var ctx *ministore.Ministore
+	var ctx *ministore.MiniStore
 	payload := []byte("{}")
-	resp, err := store.StoreGetStoreInfo(ctx, payload)
+	resp, err := store.GetStoreInfo(ctx, payload)
 	fmt.Println(resp, err)
 }
--- apis/store/store.go
+++ apis/store/store.go
@@ -1,8 +1,27 @@
+// Copyright 2020 FastWeGo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package store 店铺接口
 package store
 
+import (
+	"bytes"
+	"github.com/fastwego/ministore"
+)
+
 const (
-	apiStoreGetStoreInfo = "/product/store/get_info"
+	apiGetStoreInfo = "/product/store/get_info"
 )
 
 /*
@@ -11,6 +30,6 @@ const (
 See: https://developers.weixin.qq.com/doc/ministore/minishopopencomponent/API/store/get_store_info.html
 POST https://api.weixin.qq.com/product/store/get_info?access_token=xxxxxxxxx
 */
-func StoreGetStoreInfo(ctx *ministore.Ministore, payload []byte) (resp []byte, err error) {
-	return ctx.Client.HTTPPost(apiStoreGetStoreInfo, bytes.NewReader(payload), "application/json;charset=utf-8")
+func GetStoreInfo(ctx *ministore.MiniStore, payload []byte) (resp []byte, err error) {
+	return ctx.Client.HTTPPost(apiGetStoreInfo, bytes.NewReader(payload), "application/json;charset=utf-8")
 }
